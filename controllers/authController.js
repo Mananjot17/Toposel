b6dfe7901945d7d864918040ca030abdcc05a489
mananjot-17 <mananjot.singh@aimlarger.com>
2025-02-13 23:31:19 +0530

    implemented auth routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,80 @@
-export const login = async (req, res) => {};
+import generateTokenAndSetCookie from "../Utils/generateToken.js";
+import User from "../models/User.js";
+import bcryptjs from "bcryptjs";
 
-export const register = async (req, res) => {};
+export const register = async (req, res) => {
+  try {
+    const { username, fullName, password, dateOfBirth, gender, country } =
+      req.body;
+
+    const user = await User.findOne({ username });
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    //HASH password here
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+      fullName,
+      gender,
+      dateOfBirth,
+      country,
+    });
+
+    if (newUser) {
+      //generate JWT Token
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        dateOfBirth: newUser.dateOfBirth,
+        gender: newUser.gender,
+        country: newUser.country,
+      });
+    } else {
+      res.status(400).json({ error: "invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in register controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid Credentials" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      dateOfBirth: user.dateOfBirth,
+      gender: user.gender,
+      country: user.country,
+    });
+  } catch (error) {
+    console.log("Error in login controller:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};

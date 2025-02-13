9b02e78d88c1d9b70a97a82c2774ccb0a6758be2
mananjot-17 <mananjot.singh@aimlarger.com>
2025-02-14 00:27:48 +0530

    added protectRoute to authorised the loged in user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { searchUser } from "../controllers/userController.js";
+import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.get("/search", searchUser);
+router.get("/search", protectRoute, searchUser);
 
 export default router;

9735092ff3bd015310cfa6d5b44c465740008ae2
mananjot-17 <mananjot.singh@aimlarger.com>
2025-02-13 21:10:01 +0530

    setting up auth routes and controllers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectToMongoDB from "./db/connectToMongoDb.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   // root route
